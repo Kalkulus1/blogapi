65339e6f041355fc4b55a628b6226b093aa71c3a
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-19 05:48:46 +0000

    New updates.

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -19,7 +19,15 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::prefix('/user')->group(function(){
-    Route::post('login', 'App\Http\Controllers\Api\V1\AuthController@login');
+    Route::post('login', [App\Http\Controllers\Api\V1\AuthController::class, 'login']);
     Route::post('register', 'App\Http\Controllers\Api\V1\AuthController@register');
+});
+
+Route::middleware('auth:api')->group(function () {
     Route::post('logout', 'App\Http\Controllers\Api\V1\AuthController@logout');
+
+    Route::get('/posts', function () {
+        $response = ['message' => 'Posts Page'];
+        return response($response, 200);
+    });
 });
